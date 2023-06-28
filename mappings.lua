db96fe6cedd36c3c7960617804474b35d80033aa
André Ogle <ogleandre@gmail.com>
2023-06-28 16:45:03 +0200

    Fix typo

--- mappings.lua
+++ mappings.lua
@@ -9,7 +9,7 @@ M.custom = {
     ["Q"] = { "<nop>", "Do nothing with Q" },
     ["Y"] = { "Y", "Copy full line" },
 
-    ["<neader>hs"] = { ":split<CR>", "Split buffer horizontally" },
+    ["<leader>hs"] = { ":split<CR>", "Split buffer horizontally" },
     ["<leader>rr"] = { ":NvimTreeFindFile<CR>", "Find the current file in nvim-tree" },
     ["<leader>s"] = { ":vsplit<CR>", "Split buffer vertically" },
 
