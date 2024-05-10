ebf4bd0d57f201cdd66382729af7f49ebb77a356
André Ogle <ogleandre@gmail.com>
2024-05-10 16:03:42 +0100

    Add rust-analyzer LSP

--- configs/lsp.lua
+++ configs/lsp.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "cssls", "elixirls", "html", "tailwindcss", "tsserver" }
+local servers = { "cssls", "elixirls", "html", "rust_analyzer", "tailwindcss", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -16,6 +16,27 @@ lspconfig.elixirls.setup {
   cmd = { "/home/andre/Development/elixir-ls/scripts/language_server.sh" }
 }
 
+-- Rust
+lspconfig.rust_analyzer.setup({
+  on_attach = function(client, bufnr)
+      vim.lsp.inlay_hint.enable(bufnr)
+  end,
+
+  settings = {
+    ["rust-analyzer"] = {
+      imports = {
+        granularity = { group = "module" },
+        prefix = "self",
+      },
+      cargo = {
+        buildScripts = { enable = true },
+      },
+      procMacro = { enable = true },
+    }
+  }
+})
+
+-- Tailwind CSS
 lspconfig.tailwindcss.setup {
-  filetypes = { "astro", "html", "html-eex", "heex", "markdown", "css", "less", "postcss", "sass", "scss", "javascriptreact", "reason", "typescriptreact", "vue", "svelte" }
+  filetypes = { "astro", "html", "html-eex", "heex", "markdown", "css", "less", "postcss", "sass", "scss", "reason", "vue", "svelte" }
 }
